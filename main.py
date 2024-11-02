5072ce25874dc3bb855895a8c1b4ff095453c8d9
noyapoyo <145975078+noyapoyo@users.noreply.github.com>
2024-11-02 19:58:30 +0800

    Update main.py
    我增加了圖片轉ascii功能，可以直接把ascii藝術圖輸出在terminal

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello World!\n")
+from PIL import Image
+ASCII_CHARS = "@%#*+=-:. "
+def resize_gray_image(image, new_width=100):
+    width, height = image.size
+    aspect_ratio = height / width
+    new_height = int(aspect_ratio * new_width * 0.5)
+    resized_image = image.resize((new_width, new_height))
+    gray_image = resized_image.convert("L")
+    return gray_image
+def image_to_ascii(image):
+    pixels = image.getdata()
+    ascii_str = "".join([ASCII_CHARS[pixel // 25] for pixel in pixels])
+    ascii_image = [ascii_str[index: index + image.width] for index in range(0, len(ascii_str), image.width)]
+    return "\n".join(ascii_image)
+
+
+image_path = '/home/scott0227/Interesting project/omuba_1.jpeg'
+try:
+    image = Image.open(image_path)
+except Exception as e:
+    print(f"Error opening image: {e}")
+else:
+    gray_image = resize_gray_image(image)
+    ascii_art = image_to_ascii(gray_image)
+    print(ascii_art)
